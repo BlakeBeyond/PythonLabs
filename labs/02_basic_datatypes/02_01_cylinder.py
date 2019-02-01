9019a3e02d20f1a9e39d08304001fa8233353c60
Blake <blake.lezenski@gmail.com>
2019-02-01 15:07:07 +0100

    01_python_fundamentals

---
+++ labs/02_basic_datatypes/02_01_cylinder.py
@@ -0,0 +1,10 @@
+'''
+Write the necessary code calculate the volume and surface area
+of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
+
+
+'''
+vol = 3.14**3*5
+surface = 3.14**2*2+2*3.14*5
+Print (vol, surface)
+
---
+++ labs/02_basic_datatypes/02_03_runner.py
@@ -0,0 +1,8 @@
+'''
+
+If a runner runs 10 miles in 30 minites and 30 seconds,
+What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
+
+'''
+average_speed=16/0.55
+print(average_speed)
